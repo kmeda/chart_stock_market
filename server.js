049dca768d371f7044972fb3c54efd092c6073b5
kmeda <karthikreddymeda@gmail.com>
2017-12-06 11:12:37 +0530

    bg texture + heroku postinstall script

--- server.js
+++ server.js
@@ -8,20 +8,11 @@ var favicon = require('serve-favicon');
 const app = express();
 app.use(favicon(path.join(__dirname + '/favicon.ico')));
 
+app.use(express.static('dist'));
 
-if (process.env.NODE_ENV !== 'production') {
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpack = require('webpack');
-  const webpackConfig = require('./webpack.config.js');
-  console.log("Runnig non-prod");
-  app.use(webpackMiddleware(webpack(webpackConfig)));
-} else {
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
-  app.use(express.static('dist'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-  });
-}
 
 app.listen(process.env.PORT || 3050, () => console.log('Listening'));
